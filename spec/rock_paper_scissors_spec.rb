7502ac3b481309c59948fe54c2793faa9a46e870
Natalia Stanko <anithaly@gmail.com>
2021-10-09 16:19:42 +0200

    rock beats scissors and scissors beat paper

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -5,5 +5,16 @@ require_relative '../rock_paper_scissors'
 RSpec.describe 'rock paper scissors game' do
   it 'paper beats rock' do
     expect(RockPaperScissors.new.winner(:rock, :paper)).to eq :paper
+    expect(RockPaperScissors.new.winner(:paper, :rock)).to eq :paper
+  end
+
+  it 'scissors beat paper' do
+    expect(RockPaperScissors.new.winner(:scissors, :paper)).to eq :scissors
+    expect(RockPaperScissors.new.winner(:paper, :scissors)).to eq :scissors
+  end
+
+  it 'rock beats scissors' do
+    expect(RockPaperScissors.new.winner(:rock, :scissors)).to eq :rock
+    expect(RockPaperScissors.new.winner(:scissors, :rock)).to eq :rock
   end
 end
