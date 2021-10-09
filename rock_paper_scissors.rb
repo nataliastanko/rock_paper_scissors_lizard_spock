393932a44d21759c87d0ec65b71dbdbab7e24f24
Natalia Stanko <anithaly@gmail.com>
2021-10-09 02:12:51 +0200

    first test for rock-paper-scissors

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# https://www.codewars.com/kata/5672a98bdbdd995fad00000f
+# Rock Paper Scissors
+# Let's play! You have to return which player won! In case of a draw return Draw!.
+
+# Examples:
+
+# rps('scissors','paper') // Player 1 won!
+# rps('scissors','rock') // Player 2 won!
+# rps('paper','paper') // Draw!
---
+++ spec/rock_paper_scissors_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative '../rock_paper_scissors'
+
+RSpec.describe 'rock paper scissors game players' do
+  it 'chooses to play with the rock and gets beaten by the paper' do
+  end
+end
