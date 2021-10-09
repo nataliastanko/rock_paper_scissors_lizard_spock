20a31655b05a464baad2f928cf555bc07cfc0618
Natalia Stanko <anithaly@gmail.com>
2021-10-09 16:13:37 +0200

    present the winner

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -9,3 +9,17 @@
 # rps('scissors','paper') // Player 1 won!
 # rps('scissors','rock') // Player 2 won!
 # rps('paper','paper') // Draw!
+
+# require 'pry'
+
+##
+# Describes RockPaperScissors game
+class RockPaperScissors
+  WINS = { paper: :rock, scissors: :paper, rock: :scissors }.freeze
+  # WINS = { 'paper' => 'paper', 'scissors' => 'paper', 'rock' => 'scissors' }.freeze
+
+  def winner(choice1, choice2)
+    # binding.pry
+    WINS[choice1] == choice2 ? choice1 : choice2
+  end
+end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -2,8 +2,8 @@
 
 require_relative '../rock_paper_scissors'
 
-RSpec.describe 'rock paper scissors game players' do
-  it 'chooses to play with the rock and gets beaten by the paper' do
-    expect(1).to eq 0
+RSpec.describe 'rock paper scissors game' do
+  it 'paper beats rock' do
+    expect(RockPaperScissors.new.winner(:rock, :paper)).to eq :paper
   end
 end
