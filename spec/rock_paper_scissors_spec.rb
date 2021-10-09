8668b5f132eaac35307b6b6e1f70beacb1b54884
Natalia Stanko <anithaly@gmail.com>
2021-10-09 02:33:38 +0200

    run rspec in subdirs

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -4,5 +4,6 @@ require_relative '../rock_paper_scissors'
 
 RSpec.describe 'rock paper scissors game players' do
   it 'chooses to play with the rock and gets beaten by the paper' do
+    expect(1).to eq 0
   end
 end
